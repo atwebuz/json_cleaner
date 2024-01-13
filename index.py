44143fc477d1416a27d5727a0d2feff0d161205a
atwebuz <abdusattorov2004@mail.ru>
2024-01-13 20:31:10 +0500

    connecting sql

--- index.py
+++ index.py
@@ -1,6 +1,9 @@
 import pandas as pd
+from sqlalchemy import create_engine
+from datetime import datetime
 import json
-from tabulate import tabulate
+
+import tabulate
 
 # Assuming 'result.json' contains the JSON data
 with open('result.json', 'r', encoding='utf-8') as file:
@@ -23,19 +26,33 @@ for message in messages:
     if photo is None:
         print(f"Skipping element with no 'photo': {message}")
         continue
-    
+
+    date_str = message.get('date', None)
+    created_at = datetime.strptime(date_str, '%Y-%m-%dT%H:%M:%S').strftime('%Y-%m-%d %H:%M:%S')
+
     result_list.append({
         'id': message.get('id', None),
+        'title': message.get('text', None),  # Change 'text' to 'title'
         'photo': photo,
-        'text': message.get('text', None),
-        'date': message.get('date', None)
+        'cat_id': 1,
+        'brand_id': 1,
+        'created_at': created_at,  # Use the converted datetime string
+        'updated_at': datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S')  # Set 'updated_at' to the current UTC time
     })
 
 # Creating a new DataFrame with the extracted data
 result_df = pd.DataFrame(result_list)
 
 # Displaying the DataFrame using tabulate
-print(tabulate(result_df, headers='keys', tablefmt='pretty', showindex=False))
+print(tabulate.tabulate(result_df, headers='keys', tablefmt='pretty', showindex=False))
 
-# Saving the DataFrame to a new JSON file
-result_df.to_json('result_output.json', orient='records', lines=True)
+# SQLAlchemy engine to connect to PostgreSQL
+engine = create_engine('postgresql://postgres:1111@localhost:5432/adminLte_logistic')
+
+# Storing the DataFrame to PostgreSQL
+try:
+    # Storing the DataFrame to PostgreSQL
+    result_df.to_sql('products', con=engine, if_exists='replace', index=False)
+    print("Data inserted successfully.")
+except Exception as e:
+    print(f"Error inserting data: {e}")
